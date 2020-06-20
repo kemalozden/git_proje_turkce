455a94068f118a461b6c2031ca515da4d67f1bcf
Kemal <kemalozden@hotmail.com>
2020-06-20 19:45:21 +0100

    third commit from local repository

--- src/firstpackage/FirstClass.java
+++ src/firstpackage/FirstClass.java
@@ -5,6 +5,7 @@ public class FirstClass {
 	public static void main(String[] args) {
 		System.out.println("First commit from local repository");
 System.out.println("Second commit from local repository");
+	System.out.println("third commit from local repository. yalniz bu is SIKICI olmaya basladi");
 	}
 
 }
