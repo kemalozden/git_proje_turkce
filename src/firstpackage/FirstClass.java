18a2f0318c4eaeb90cabf4f67f5ed46929facf6f
kemalozden <62070522+kemalozden@users.noreply.github.com>
2020-06-20 20:16:03 +0100

    Update FirstClass.java
    First commit from remote repository

--- src/firstpackage/FirstClass.java
+++ src/firstpackage/FirstClass.java
@@ -6,6 +6,9 @@ public class FirstClass {
 		System.out.println("First commit from local repository");
 System.out.println("Second commit from local repository");
 	System.out.println("third commit from local repository. yalniz bu is SIKICI olmaya basladi");
+		System.out.println("first commit from github to local repository");
+		System.out.println("8.18  pm tired and hungry and bored");
+		
 	}
 
 }
